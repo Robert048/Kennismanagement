a461a9395076a97ce1fa667c73f390db063ce4c0
maaike1193 <maaikeveurink@hotmail.com>
2016-06-09 12:58:30 +0200

    profiel berichten

--- WorQit/Website/php/View/profiel.php
+++ WorQit/Website/php/View/profiel.php
@@ -251,16 +251,6 @@ if ($_SESSION['isloggedin']) {
     </script>
     </body>
     </html>
-    <script src="../../dashgum/Theme/assets/js/jquery.js"></script>
-    <script src="../../dashgum/Theme/assets/js/bootstrap.min.js"></script>
-    <script src="../../dashgum/Theme/assets/js/jquery-ui-1.9.2.custom.min.js"></script>
-    <script src="../../dashgum/Theme/assets/js/jquery.ui.touch-punch.min.js"></script>
-    <script class="include" type="text/javascript"
-            src="../../dashgum/Theme/assets/js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="../../dashgum/Theme/assets/js/jquery.scrollTo.min.js"></script>
-    <script src="../../dashgum/Theme/assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-    <!--common script for all pages-->
-    <script src="../../dashgum/Theme/assets/js/common-scripts.js"></script>
 <?php
     }
     else{
