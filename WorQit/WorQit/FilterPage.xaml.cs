e9945c88bdde8dd7ad595b53b83be15e7d610de9
robert048 <robert>
2016-06-09 12:31:10 +0200

    zooi

---
+++ WorQit/WorQit/FilterPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+
+namespace WorQit
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FilterPage : Page
+    {
+        public FilterPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
+
+//api/Vacancy/getVacancies?function={function}&salary={salary}&hours={hours}&requirements={requirements}&tags={tags}&location={location}
