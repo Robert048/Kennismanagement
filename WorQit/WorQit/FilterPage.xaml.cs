85de1f74a2b14e9c152f30d2f45528942c56d2bc
robert048 <robert>
2016-06-17 15:24:08 +0200

    Comments

--- WorQit/WorQit/FilterPage.xaml.cs
+++ WorQit/WorQit/FilterPage.xaml.cs
@@ -3,10 +3,11 @@
 namespace WorQit
 {
     /// <summary>
-    /// 
+    /// Pagina om filters in te stellen
     /// </summary>
     public sealed partial class FilterPage : Page
     {
+        //lokale instellingen van UWP om instellingen op te slaan.
         Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
 
         public FilterPage()
@@ -14,11 +15,13 @@ namespace WorQit
             this.InitializeComponent();
         }
 
+        //back button om terug te gaan naar hoofdscherm
         private void btnBack_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Start));
         }
 
+        //filters toevoegen aan de localsettings
         private void btnApply_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             localSettings.Values["distance"] = txtDistance.Text;
